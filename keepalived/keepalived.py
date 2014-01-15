e676c0541e296675cf92f7f1f82e4e6d4da4dc95
Pablo Sanchez <root@automation.stackops.int>
2014-01-15 10:09:23 +0000

    Updated Keepalived

--- keepalived/keepalived.py
+++ keepalived/keepalived.py
@@ -37,3 +37,57 @@ def uninstall():
 def install():
     package_ensure('keepalived')
 
+
+def configure():
+    sudo('echo "net.ipv4.ip_nonlocal_bind=1" >> /etc/sysctl.conf')
+    sudo('sysctlp -p')
+    
+    keepalived_conf = text_strip_margin('''
+	    |# Configuration File for keepalived
+	    |global_defs {
+	    |# each load balancer should have a different ID
+	    |# this will be used in SMTP alerts, so you should make
+	    |# each router easily identifiable
+	    |lvs_id LB1
+	    |}
+	    |
+	    |#health-check for keepalive
+	    |vrrp_script chk_nginx { # Requires keepalived-1.1.13
+	    |	#script "killall -0 nginx" # cheaper than pidof
+	    |	script ''"pidof nginx"'' 
+	    |	interval 2 # check every 2 seconds
+	    |	weight 2 # add 2 points of prio if OK
+	    |}
+	    |
+	    |vrrp_instance VI_1 {
+	    |	state MASTER
+	    |	interface eth0
+	    |	  
+	    |	# each virtual router id must be unique per instance name#
+	    |	virtual_router_id 51
+	    |	# MASTER and BACKUP state are determined by the priority
+	    |	# even if you specify MASTER as the state, the state will
+	    |	# be voted on by priority (so if your state is MASTER but 
+	    |	# your priority is lower than the router with BACKUP, you 
+	    |	# will lose the MASTER state)
+	    |   priority 101
+	    |
+	    |	#check if we are still running
+	    |	track_script {
+	    |		chk_nginx
+	    |	}
+	    |	
+	    |	# these are the IP addresses that keepalived will setup on 
+	    |	# this without this block, keepalived will not setup and 
+	    |	# takedown the IP addresses
+	    |	virtual_ipaddress {
+	    |	172.22.23.200
+	    |	}
+	    |}
+	    |''' 
+            )
+    sudo('''echo '%s' > /etc/keepalived/keepalived.conf''' % keepalived_conf)
+
+
+
+
